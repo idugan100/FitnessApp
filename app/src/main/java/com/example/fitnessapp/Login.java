63a9edd6262a7d6161a73a1c04b9daa98e41dfcb
idugan100 <isaacd4444@gmail.com>
2024-04-08 15:44:02 -0400

    adding links between signup and login pages

--- app/src/main/java/com/example/fitnessapp/Login.java
+++ app/src/main/java/com/example/fitnessapp/Login.java
@@ -1,6 +1,8 @@
 package com.example.fitnessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void signUpButtonLink(View view) {
+        Intent i = new Intent(this, SignUp.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/fitnessapp/SignUp.java
+++ app/src/main/java/com/example/fitnessapp/SignUp.java
@@ -1,6 +1,8 @@
 package com.example.fitnessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class SignUp extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void clickLoginLink(View view) {
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
     }
 }
