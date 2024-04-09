527c4c75fabdf40f711eeae43f06f769c2a01efd
idugan100 <isaacd4444@gmail.com>
2024-04-09 01:19:34 -0400

    adding buttons to admin panel

--- app/src/main/java/com/example/fitnessapp/AdminPanel.java
+++ app/src/main/java/com/example/fitnessapp/AdminPanel.java
@@ -1,6 +1,8 @@
 package com.example.fitnessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class AdminPanel extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void backToHome(View view) {
+        Intent i = new Intent(this, UserHome.class);
+        startActivity(i);
     }
 }
