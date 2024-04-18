da6e95db2ec1c471628702f7a6cee6b1f2ad16bd
idugan100 <isaacd4444@gmail.com>
2024-04-17 21:32:07 -0400

    adding activities now works

--- app/src/main/java/com/example/fitnessapp/AllNotifications.java
+++ app/src/main/java/com/example/fitnessapp/AllNotifications.java
@@ -17,6 +17,7 @@ public class AllNotifications extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_all_notifications);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/fitnessapp/NewActivity.java
+++ app/src/main/java/com/example/fitnessapp/NewActivity.java
@@ -1,8 +1,14 @@
 package com.example.fitnessapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +16,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NewActivity extends AppCompatActivity {
+    CalendarView cal;
+    Spinner activity;
+    EditText duration;
+    Spinner intensity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new);
+        cal = findViewById(R.id.calender);
+        activity = findViewById(R.id.activity);
+        duration = findViewById(R.id.duration);
+        intensity = findViewById(R.id.intensity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -30,7 +64,63 @@ public class NewActivity extends AppCompatActivity {
     }
 
     public void createActivity(View view) {
-        //send request to the backend to create and activity here
-        this.backToHome(view);
+        //get values from form
+        Date date = new Date(cal.getDate());
+        Log.d("debug",date.toString());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        Instant instant = date.toInstant();
+        Log.d("debug",instant.toString());
+
+
+        String activityName = activity.getSelectedItem().toString();
+        String intensityString = intensity.getSelectedItem().toString();
+        int durationValue = Integer.parseInt(duration.getText().toString());
+
+        //create json
+        JSONObject body = new JSONObject();
+        try {
+            body.put("name",activityName);
+            body.put("intensity",intensityString);
+            body.put("duration",durationValue);
+            body.put("date",instant.toString());
+        }catch (Exception error){
+            Toast.makeText(getApplicationContext(), "json error:"+ error.getMessage(), Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //pass json and token to a request factory
+        String url = "http://18.226.82.203:8080/activities/"+User.getInstance().getId();
+        Context c = this;
+
+        //send request
+
+        StringRequest req = new StringRequest(
+                Request.Method.POST,url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Intent i = new Intent(NewActivity.this,AllActivities.class);
+                        startActivity(i);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "error:"+ error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer "+ User.getInstance().getToken());
+                return params;
+            }
+            public byte[] getBody() {
+                // Override this method to send the JSON body
+                return body.toString().getBytes();
+            }};
+        Volley.newRequestQueue(this).add(req);
+
     }
 }
