74da350512ca6c9b0daf038580ffe9e003e9a320
SpencerPresley <SpencerPresley96@gmail.com>
2024-04-30 14:55:44 -0400

    heatmap class intial setup

---
+++ app/src/main/java/com/example/fitnessapp/HeatMap.java
@@ -0,0 +1,44 @@
+//package com.example.fitnessapp;
+//
+//public class HeatMap {
+//    enum Intensity {
+//        NONE,
+//        LOW,
+//        MODERATE,
+//        HIGH,
+//        EXTREME,
+//    }
+//
+//    private
+//
+//    private void setColor (Intensity i) {
+//        /*
+//            Passed an intensity
+//            Sets value on heatmap to the intensity
+//
+//            NONE = 0 LOW = 1, MODERATE = 2, HIGH = 3
+//            NONE indicates no activity was completed that day
+//
+//            For now only raw value displayed
+//
+//            Will convert -> 0 = Green, 1 = Yellow, 2 = Red
+//         */
+//
+//        // 2d array 7x4 to represent 4 weeks
+//        // make request /activities/stats/all
+//        // grab the dates within the last 30 days
+//        // grab the minutes and put them in box
+//        // 0 min = NONE, < 30 = LOW (pastel), 30-60 = MODERATE (color), > 60 < 120 = HIGH
+//        // > 120 = EXTREME
+//
+//    }
+//
+////    private String getColor (Intensity i) {
+////        if (i == Intensity.LOW) {
+////            return "Green";
+////        } else if (i == Intensity.MODERATE) {
+////            return "Yellow";
+////        }
+////        return "Red";
+////    }
+//}
